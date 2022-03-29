4b2729034df58f249adcb1d72c53f7d1513e5860
mahaoyuHKU <84957752+mahaoyuHKU@users.noreply.github.com>
2022-03-29 21:59:37 +0800

    Delete CSwin directory

--- CSwin/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# ------------------------------------------
-# CSWin Transformer
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-# written By Xiaoyi Dong
-# ------------------------------------------
-
-from .cswin_boat import *
